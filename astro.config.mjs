8db58998153eedb086b0ce735d6953235e6a6a77
risyalf <risyalfebrianto97@gmail.com>
2025-12-14 21:26:16 +0700

    deploy vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,26 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-
+import vercel from "@astrojs/vercel/serverless";
 const DEV_PORT = 2121;
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.CI
-		? 'https://themesberg.github.io'
-		: `http://localhost:${DEV_PORT}`,
-	base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  site: process.env.CI ? 'https://themesberg.github.io' : `http://localhost:${DEV_PORT}`,
+  base: process.env.CI ? '/flowbite-astro-admin-dashboard' : undefined,
+  // output: 'server',
 
-	// output: 'server',
+  /* Like Vercel, Netlify,… Mimicking for dev. server */
+  // trailingSlash: 'always',
 
-	/* Like Vercel, Netlify,… Mimicking for dev. server */
-	// trailingSlash: 'always',
-
-	server: {
-		/* Dev. server only */
-		port: DEV_PORT,
-	},
-
-	integrations: [
-		//
-		sitemap(),
-		tailwind(),
-	],
+  server: {
+    /* Dev. server only */
+    port: DEV_PORT
+  },
+  integrations: [
+  //
+  sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
